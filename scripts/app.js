6f2399a70e30572a4c8605e4b98d5602c23d74d3
oliverlangmo <oliver.langmo@gmail.com>
2016-06-05 19:01:21 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,60 @@
+var classInfo;
+var studentNum = 0;
+$(document).ready(function() {
+  $('#getAjax' ).click( function(){
+    console.log( 'button clicked' );
+     $.ajax({
+       url: 'https://raw.githubusercontent.com/devjanaprime/2.4-jQueryAjaxJSON/master/students.json',
+       dataType: 'json',
+       success: function( data ){
+          console.log( 'in ajax success' );
+          console.log( data );
+          classInfo = data;
+          showStudents();
+         }, // end success
+       statusCode: {
+          404: function(){
+             alert( 'error connecting to server' );
+          } // end 404
+         } // end statusCode
+       }); // end ajax  object
+});
+
+}); // end click getJSONAjax button
+
+var showStudents = function(){
+  var student = classInfo.students[studentNum];
+  $('#display').html('<p> First Name: '+ student.first_name + '<br /> Last Name: ' + student.last_name +
+  '<br /> City: ' + student.city  + '<br /> Shout Out: ' + student.shoutout + '</p>').css({"width":"380px","text-align":"right",
+  "background-repeat":"no-repeat","background-image": "url(img/" + studentNum + ".jpg)"});
+  $('#display').append((studentNum+1) + ' of 20');
+  var viewDiv = document.createElement('div');
+  viewDiv.textContent = '';
+  viewDiv.className = 'new';
+  document.getElementById('display').appendChild(viewDiv);
+  var prevButton = document.createElement('button');
+  prevButton.textContent = 'Previous Student';
+  prevButton.className = 'prev';
+  viewDiv.appendChild(prevButton);
+  var nextButton = document.createElement('button');
+  nextButton.textContent = 'Next Student';
+  nextButton.className = 'next';
+  viewDiv.appendChild(nextButton);
+  $('.prev').click(function() {
+      if (studentNum === 0) {
+        studentNum = 20;
+      }
+      $('#new').remove();
+      studentNum--;
+      showStudents();
+    });
+
+    $('.next').click(function() {
+      if (studentNum == classInfo.students.length-1) {
+          studentNum = -1;
+        }
+      $('#new').remove();
+      studentNum++;
+      showStudents();
+    });
+};
